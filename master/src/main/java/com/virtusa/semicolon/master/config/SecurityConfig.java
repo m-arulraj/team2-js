df857003b7fc42dd3a7df57c452ae6170643ffb0
shubham88714 <shubham003.shubh@gmail.com>
2019-03-19 19:41:52 +0530

    commiting data

---
+++ master/src/main/java/com/virtusa/semicolon/master/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.virtusa.semicolon.master.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		/* auth.jdbcAuthentication().dataSource(dataSource); */
+
+		/*
+		 * UserBuilder users = User.withDefaultPasswordEncoder();
+		 * auth.inMemoryAuthentication().withUser(users.username("shubham").
+		 * password("123").roles("EMPLOYEE"))
+		 * .withUser(users.username("nagar").password("123").roles("EMPLOYEE",
+		 * "MANAGER"))
+		 * .withUser(users.username("Sahil").password("123").roles("EMPLOYEE",
+		 * "ADMIN"));
+		 */
+
+		auth.inMemoryAuthentication().withUser("user").password("123").roles("USER")
+		.and().withUser("emp").password("123").roles("EMPLOYER")
+				.and().withUser("seek").password("123").roles("SEEKER");
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/*
+		 * http.authorizeRequests().anyRequest().authenticated()
+		 * .antMatchers("/**").hasRole("EMPLOYEE")
+		 * .antMatchers("/leaders/**").hasRole("MANAGER")
+		 * .antMatchers("/systems/**").hasRole("ADMIN") .and()
+		 * .formLogin().loginPage("/customLoginPage")
+		 * .loginProcessingUrl("/authenticateTheUser").permitAll().and()
+		 * .logout().permitAll()
+		 * .and().exceptionHandling().accessDeniedPage("/access-denied");
+		 */
+		http.authorizeRequests().anyRequest().authenticated().antMatchers("/**").hasRole("USER")
+		.and().formLogin()/*.loginPage("/login-page")
+		.loginProcessingUrl("/authenticateUser")*/.permitAll()
+		.and().logout().permitAll();
+
+	}
+}
