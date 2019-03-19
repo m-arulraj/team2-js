1fd51832b4b30a5b2a5192b0afcab04b49dfc1c3
SrinivasM2606 <seenu2378@gmail.com>
2019-03-19 11:52:45 +0530

    Added Jobseeker Service

---
+++ jobseeker-service/src/main/java/com/virtusa/semicolon/jobseeker_service/App.java
@@ -0,0 +1,13 @@
+package com.virtusa.semicolon.jobseeker_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
