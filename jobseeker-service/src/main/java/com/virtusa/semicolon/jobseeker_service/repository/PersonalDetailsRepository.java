971fcbb82d9c4f7d5fdb67a28d2d0c901aaa8394
SrinivasM2606 <seenu2378@gmail.com>
2019-03-20 17:36:44 +0530

    updated jobseeker service

---
+++ jobseeker-service/src/main/java/com/virtusa/semicolon/jobseeker_service/repository/PersonalDetailsRepository.java
@@ -0,0 +1,8 @@
+package com.virtusa.semicolon.jobseeker_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonalDetailsRepository extends JpaRepository<PersonalDetailsRepository, String>
+{
+	
+}
