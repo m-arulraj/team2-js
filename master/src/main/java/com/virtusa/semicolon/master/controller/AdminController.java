eff3c661e9c768003e1ba6fd7a991ee82e990a76
shubham88714 <shubham003.shubh@gmail.com>
2019-03-21 20:00:08 +0530

    admin service view pages completed

--- master/src/main/java/com/virtusa/semicolon/master/controller/AdminController.java
+++ master/src/main/java/com/virtusa/semicolon/master/controller/AdminController.java
@@ -2,14 +2,41 @@ package com.virtusa.semicolon.master.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
+@RequestMapping("/admin")
 public class AdminController {
 
-	@GetMapping("/admin")
+	// homepage
+	@GetMapping("")
 	public String adminHomePage() {
 		return "admin-home";
+	}
+
+	// request approval
+	@GetMapping("/requests")
+	public String adminApprovalRequest() {
+		return "admin-approval-request";
+	}
+
+	// reports
+	@GetMapping("/reports")
+	public String getReports() {
+		return "admin-reports";
+	}
+
+	// profile
+	@GetMapping("/profile")
+	public String updateProfile() {
+		return "admin-profile";
+	}
+
+	//block
+	@GetMapping("/users/block")
+	public String blockUser() {
+		return "admin-block";
 	}
 
 }
