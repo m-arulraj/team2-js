e45d7beff698aaedcba2b7f88ed3c4b676c8a5a3
VamsiKrishnaKomati <vamsikrishna244@gmail.com>
2019-03-21 15:48:34 +0530

    employer service modified

--- employer-service/src/main/java/com/virtusa/semicolon/employer_service/repository/PostedJobsListRepository.java
+++ employer-service/src/main/java/com/virtusa/semicolon/employer_service/repository/PostedJobsListRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.virtusa.semicolon.employer_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,23 +10,3 @@ import com.virtusa.semicolon.employer_service.domain.PostedJobsList;
 public interface PostedJobsListRepository extends JpaRepository<PostedJobsList,Long>{
 
 }
-=======
-package com.virtusa.semicolon.employer_service.repository;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import com.virtusa.semicolon.employer_service.domain.PostedJobsList;
-
-@Repository
-
-public interface PostedJobsListRepository extends JpaRepository<PostedJobsList,Long>{
-
-	@Query("select p.jobId from PostedJobsList p where p.jobTitle=:jobTitle")
-	public List<String> managingJobList(@Param("jobTitle") String jobTitle);
-}
->>>>>>> 908a1bf75b7469635f0b7d499725f6ff684b05df
--- employer-service/src/main/java/com/virtusa/semicolon/employer_service/service/EmployerService.java
+++ employer-service/src/main/java/com/virtusa/semicolon/employer_service/service/EmployerService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.virtusa.semicolon.employer_service.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,35 +21,3 @@ public class EmployerService {
 
 
 }
-=======
-package com.virtusa.semicolon.employer_service.service;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.virtusa.semicolon.employer_service.domain.PostedJobsList;
-import com.virtusa.semicolon.employer_service.repository.PostedJobsListRepository;
-
-@Service
-public class EmployerService {
-	
-	@Autowired
-	PostedJobsListRepository postedJobsListRepository;
-	
-	@Transactional
-	public PostedJobsList postingJob(PostedJobsList postedJobsList){
-		return postedJobsListRepository.save(postedJobsList);
-	}
-
-	public List<String> managingJobList(String id) {
-		System.out.println(id);
-		return postedJobsListRepository.managingJobList(id);
-	}
-	
-
-	
-}
->>>>>>> 908a1bf75b7469635f0b7d499725f6ff684b05df
