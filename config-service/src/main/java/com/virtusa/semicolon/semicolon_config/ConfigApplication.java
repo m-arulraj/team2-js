18b928996b312621c1ac3651c377e462d7cf072b
shubham88714 <shubham003.shubh@gmail.com>
2019-03-25 18:47:22 +0530

    gateway updated

--- config-service/src/main/java/com/virtusa/semicolon/semicolon_config/ConfigApplication.java
+++ config-service/src/main/java/com/virtusa/semicolon/semicolon_config/ConfigApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class App 
+public class ConfigApplication 
 {
     public static void main( String[] args )
     {
-       new SpringApplicationBuilder(App.class).run(args);
+       new SpringApplicationBuilder(ConfigApplication.class).run(args);
     }
 }
